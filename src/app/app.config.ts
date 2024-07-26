a27ad7cd75549ae56ee82e1074d6af313bc85a81
Ankit <Ankitkushwaha@wattmonk.com>
2024-07-26 18:07:33 +0530

    f

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,9 +2,11 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
+import { BrowserAnimationsModule, provideAnimations } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideAnimations(),
     provideRouter(routes),
     provideHttpClient() // Add this line to include HttpClientModule
   ]
